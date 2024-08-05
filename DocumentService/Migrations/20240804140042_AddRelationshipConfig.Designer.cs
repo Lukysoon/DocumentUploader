995ab82215d6b00ade407be1e13a3fe5a2f1a57e
Lukáš Starenko <lukas.starenko@aimtec.cz>
2024-08-05 19:06:22 +0200

    Add migrations

--- DocumentService/Migrations/20240804140042_AddRelationshipConfig.Designer.cs
+++ DocumentService/Migrations/20240804140042_AddRelationshipConfig.Designer.cs
@@ -4,6 +4,7 @@ using DocumentUploader.DocumentService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DocumentService.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240804140042_AddRelationshipConfig")]
+    partial class AddRelationshipConfig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DocumentService/Migrations/20240804140042_AddRelationshipConfig.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationshipConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DocumentTag_Documents_documentsId",
+                table: "DocumentTag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DocumentTag",
+                table: "DocumentTag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DocumentTag_documentsId",
+                table: "DocumentTag");
+
+            migrationBuilder.RenameColumn(
+                name: "documentsId",
+                table: "DocumentTag",
+                newName: "DocumentsId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Tags",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Documents",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DocumentTag",
+                table: "DocumentTag",
+                columns: new[] { "DocumentsId", "TagsId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentTag_TagsId",
+                table: "DocumentTag",
+                column: "TagsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DocumentTag_Documents_DocumentsId",
+                table: "DocumentTag",
+                column: "DocumentsId",
+                principalTable: "Documents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DocumentTag_Documents_DocumentsId",
+                table: "DocumentTag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DocumentTag",
+                table: "DocumentTag");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DocumentTag_TagsId",
+                table: "DocumentTag");
+
+            migrationBuilder.RenameColumn(
+                name: "DocumentsId",
+                table: "DocumentTag",
+                newName: "documentsId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Tags",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Documents",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWID()");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DocumentTag",
+                table: "DocumentTag",
+                columns: new[] { "TagsId", "documentsId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentTag_documentsId",
+                table: "DocumentTag",
+                column: "documentsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DocumentTag_Documents_documentsId",
+                table: "DocumentTag",
+                column: "documentsId",
+                principalTable: "Documents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
