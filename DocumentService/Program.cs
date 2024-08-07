ed2d7a409cc400ca4acf438ede71da6e4a9125c5
Lukysoon <starenko.lukas@pm.me>
2024-08-07 18:22:26 +0200

    Add DI

--- DocumentService/Program.cs
+++ DocumentService/Program.cs
@@ -1,10 +1,17 @@
 using DocumentUploader.DocumentService.Data;
+using DocumentUploader.DocumentService.Repositories;
+using DocumentUploader.DocumentService.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DocumentContext") ?? throw new InvalidOperationException("Connection string 'DocumentContext' not found.")));
+
+builder.Services.AddTransient<IDocService, DocService>();
+builder.Services.AddTransient<ITagService, TagService>();
+builder.Services.AddTransient<ITagRepository, TagRepository>();
+builder.Services.AddTransient<IDocumentRepository, DocumentRepository>();
 
 builder.Services.AddControllers().AddXmlSerializerFormatters();
 builder.Services.AddEndpointsApiExplorer();
