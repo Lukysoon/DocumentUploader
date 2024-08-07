d9abd4ccd9b50dd38d206465aa92fe6740ffe7c0
Lukysoon <starenko.lukas@pm.me>
2024-08-07 18:34:47 +0200

    Change route to api convention

--- DocumentService/Controllers/DocumentController.cs
+++ DocumentService/Controllers/DocumentController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DocumentUploader.DocumentService.Controllers 
 {
     [ApiController]
-    [Route("document")]
+    [Route("api/document")]
     public class DocumentController : Controller
     {
         private readonly ITagService _tagService;
