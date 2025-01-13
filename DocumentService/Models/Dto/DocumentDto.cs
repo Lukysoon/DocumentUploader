45e2d268ecc14b825cc776c6c94ca06eff80176a
Lukas Starenko <test.mopnx@passinbox.com>
2025-01-13 11:49:32 +0300

    Add Dto validation

--- DocumentService/Models/Dto/DocumentDto.cs
+++ DocumentService/Models/Dto/DocumentDto.cs
@@ -1,9 +1,16 @@
-﻿namespace DocumentService;
+﻿using System.ComponentModel.DataAnnotations;
+using DocumentService.Validation;
+
+namespace DocumentService;
 
 public class DocumentDto
 {
+    [Required]
+    [MaxLength(50)]
     public string FileName { get; set; } = null!;
+    [IsBase64]
     public string DataInBase64 { get; set; } = null!;
+    [Required, MinLength(1, ErrorMessage = "Add at least one tag please.")]
     public List<string> Tags { get; set; } = new List<string>();
 
     public DocumentDto() {}
---
+++ DocumentService/Validation/CustomAtributes/IsBase64Attribute.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DocumentService.Validation;
+
+public class IsBase64Attribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(
+        object? value, ValidationContext validationContext)
+    {
+        var data = validationContext.ObjectInstance;
+        if (!IsBase64String((string)data))
+        {
+            return new ValidationResult($"Property {validationContext.DisplayName} must be in base64.");
+        }
+
+        return ValidationResult.Success;
+    }
+
+    private bool IsBase64String(string base64)
+    {
+        if (string.IsNullOrWhiteSpace(base64))
+        {
+            throw new Exception("Document data in base64 format is null.");
+        }
+
+        Span<byte> buffer = new Span<byte>(new byte[base64.Length]);
+        return Convert.TryFromBase64String(base64, buffer, out int bytesParsed);
+    }
+}
